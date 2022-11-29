e19ef91eb4ee51e6a70861ce2cf36b91b6e48041
Lyeonu <118809507+Lyeonu@users.noreply.github.com>
2022-11-29 14:53:51 +0900

    Update m.js

--- m.js
+++ m.js
@@ -1,11 +1,30 @@
-const express = require('express');
+import React from "react";
+function Food({name, picture}) {
+    return(
+        <div>
+            <h1>I like {name} </h1>
+            <img src = {picture} />
+        </div>
+    );
+}
+const foodILike = [
+    {
+        name:'kimchi',
+        image:'https://bit.ly/3d14HVx',
+    },
+    {
+        name:'Samgyeopsal',
+        image:'http://asq.kr/wttMFjg1IkYC',
+    }
+];
+function renderFood(dish){
+    return<Food name={dish.name} picture={dish.image} />;
+}
 
-const app = express();
-
-app.use((request, respon) => {
-    respon.send('<h1>Hello express</h1>');
-});
-
-app.listen(52273, () => {
-    console.log('Server running at http://127.0.0.1:52273');
-});
+function App(){
+    return(
+        <div>
+            {foodILike.map(renderFood)}
+        </div>
+    );
+}
